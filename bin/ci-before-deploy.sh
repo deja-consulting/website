9c685ed840b5b84c66a043cbd08700974c034942
Madoc <MadocDoyu@gmail.com>
2019-10-07 22:33:14 +0200

    Google site verification fixed for main releases. (Closes #5)

--- bin/ci-before-deploy.sh
+++ bin/ci-before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ "$TRAVIS_TAG" = "release" ]]; then
+if [[ "$TRAVIS_BRANCH" = "release/latest" ]]; then
   mkdir -p target && cp "static/other/google7d98c0f02ea8cf75.html" "target"
 fi
